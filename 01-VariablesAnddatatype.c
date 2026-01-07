437cf710c45166d2b9ec0afc73f667a27ea110e3
Geetajoshi08 <geetajoshi9621@gmail.com>
2026-01-07 22:16:42 +0530

    Add files via upload

---
+++ 01-VariablesAnddatatype.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    // interger data type
+    int age = 22;        // here age is variable
+ 
+    // floating data type
+    float height =5.6;    // height is variable
+
+    // character data type 
+    char grade = 'A';     // grade is variable
+
+    // double data type
+    double salary = 55000.75;    // salary is variable
+
+    // printing all the values
+    printf(" Age is : %d\n", age );
+    printf(" Height is : %f\n", height );
+    printf(" Grade is : %c\n", grade );
+    printf(" Salary is : %f\n", salary );
+
+    return 0;
+
+}
