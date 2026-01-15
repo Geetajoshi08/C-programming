e79609adec8a4176e986a1c7f5cc5e5078975d75
Geetajoshi08 <geetajoshi9621@gmail.com>
2026-01-15 22:32:13 +0530

    Added new program

---
+++ 04PatternPrinting/0411-StarTriangleMast.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int n; 
+    printf("Enter number of rows : ");
+    scanf("%d",&n);
+    
+    int a;
+    for(int i=1; i<=n; i++){
+        a=n;
+        for(int j=1; j<=n-i; j++){     // for spaces
+            printf("  ");
+        }
+        for(int k=1; k<=i; k++){      // for star
+            
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
