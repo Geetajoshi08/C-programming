15a4e55a28b204a4f960f9a4031b88197a927182
Geetajoshi08 <geetajoshi9621@gmail.com>
2026-01-16 09:18:38 +0530

    New program

---
+++ 04PatternPrinting/0413-AlphabetTriangleNew.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+    int n; 
+    printf("Enter number of rows : ");
+    scanf("%d",&n);
+    
+    //int a;
+    for(int i=1; i<=n; i++){
+        char ch = 'A';
+        for(int j=1; j<=n-i; j++){     // for spaces
+            printf("  ");
+        }
+        for(int k=1; k<=i; k++){      // for star
+            printf("%c " , ch);
+            ch++;
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
